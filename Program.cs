a2be489c5960989827f87419d469b165ffbe3200
Tomas Garcia Cardenas <tomass11052001@hotmail.com>
2026-04-07 15:46:02 -0300

    Proyect creation

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Practico1ProgramacionIII;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola mundo!!!");
+    }
+}
