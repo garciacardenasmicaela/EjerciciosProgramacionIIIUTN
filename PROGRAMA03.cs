292c3728bda94ef0f5e9ffd61cec72c689b03cef
Micaela Garcia Cardenas <garciacardenasmicaela@gmail.com>
2026-04-09 09:09:13 -0300

    PROGRAMA03: Ejercicios completos

--- PROGRAMA03.cs
+++ PROGRAMA03.cs
@@ -4,11 +4,17 @@
     {
         Console.WriteLine("BLOQUE 2: NÚMEROS CON DECIMALES");
 
-        double estatura = 1.85;
-        Console.WriteLine("Valor: " + estatura);
-
-        decimal precioProducto = 299.99m;
-        Console.WriteLine("Precio: $" + precioProducto);
+        double primernum = 1.85;
+        double segundonum = 1.26;
+        double tercernum = 8.25;
+        double cuartonum = 100.99;
+        double quintonum = 250.10;
+        Console.WriteLine("Valores decimales guardados:\n" +
+                          "1) " + primernum + "\n" +
+                          "2) " + segundonum + "\n" +
+                          "3) " + tercernum + "\n" +
+                          "4) " + cuartonum + "\n" +
+                          "5) " + quintonum);
         Console.ReadLine();
     }
 }
